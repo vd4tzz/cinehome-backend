983abf0eac831ed213a0c165ad4438f70200cd0b
d4tzz <d4tzz@temp.com>
2025-11-08 16:09:02 +0700

    remove CinemaOwnerShip

--- src/cinema/screen.controller.ts
+++ src/cinema/screen.controller.ts
@@ -7,21 +7,18 @@ import { UpdateScreenResponse } from "./dto/update-screen-response";
 import { PageParam } from "../common/pagination/page-param";
 import { RolesGuard } from "../auth/roles.guard";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
-import { CinemaOwnershipGuard } from "../auth/cinema-ownership.guard";
 import { Roles } from "../auth/roles.decorator";
 import { RoleName } from "../user/entity/role.entity";
 import { ApiBearerAuth } from "@nestjs/swagger";
-import { CinemaOwnership } from "../auth/cinema-ownership.decorator";
 
 @ApiBearerAuth("access-token")
 @Controller("api/cinemas/:cinemaId/screens")
-@UseGuards(JwtAuthGuard, RolesGuard, CinemaOwnershipGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ScreenController {
   constructor(private screenService: ScreenService) {}
 
   @Post()
   @Roles(RoleName.SUPER_ADMIN, RoleName.ADMIN)
-  @CinemaOwnership()
   async createScreen(
     @Param("cinemaId") cinemaId: number,
     @Body() createScreenRequest: CreateScreenRequest,
@@ -31,7 +28,6 @@ export class ScreenController {
 
   @Put(":screenId")
   @Roles(RoleName.SUPER_ADMIN, RoleName.ADMIN)
-  @CinemaOwnership()
   async updateScreen(
     @Param("cinemaId") cinemaId: number,
     @Param("screenId") screenId: number,
@@ -42,21 +38,18 @@ export class ScreenController {
 
   @Get(":screenId")
   @Roles(RoleName.SUPER_ADMIN, RoleName.ADMIN)
-  @CinemaOwnership()
   async getScreenById(@Param("screenId") screenId: number) {
     return this.screenService.getScreenById(screenId);
   }
 
   @Get()
   @Roles(RoleName.SUPER_ADMIN, RoleName.ADMIN)
-  @CinemaOwnership()
   async getScreens(@Param("cinemaId") cinemaId: number, @Query() pageParam: PageParam) {
     return this.screenService.getScreens(cinemaId, pageParam);
   }
 
   @Delete(":screenId")
   @Roles(RoleName.SUPER_ADMIN, RoleName.ADMIN)
-  @CinemaOwnership()
   async deleteScreen(
     @Param("cinemaId", ParseIntPipe) cinemaId: number,
     @Param("screenId", ParseIntPipe) screenId: number,
